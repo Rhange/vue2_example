0e1ebda182c3c22d9c5310b949d325ec79363bb6
Rhange <rhange0511@gmail.com>
2021-05-04 10:55:29 +0900

    Vue practice first commit
    1. The Vue Instance
      - Vue에서는 DOM을 Vue instance로 제어
      - tag안에 vue instance에 있는 데이터들을 render하기 위해서 `{{}}` 표기법을 사용
    
    2. Attribute Binding
      - tag의 속성에 vue instance에 있는 데이터들을 사용하기 위해서는 `v-bind:src`와 같이 `v-bind:` 표현을 사용한다.
      - short cut으로 `:`만 앞에 붙여줘도 된다.

---
+++ main.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'This socks is very popular.',
+    image: './assets/white-socks.jpg',
+    link: 'https://www.softwear.com.ng/product/white-socks',
+    inStock: true
+  }
+})
