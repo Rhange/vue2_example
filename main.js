68f1b1b46d5fafd7ce54e7cf0e3d5aca5bb2f1af
Rhange <rhange0511@gmail.com>
2021-05-04 16:49:32 +0900

    8. Components
    - `component`를 사용하면 html과 data, methods, computed 모두를 하나의 코드 블럭으로 묶을 수 있다.
    - `component`는 전체를 재사용 가능한 부분으로 각각 나눔으로써 어플리케이션 관리를 더 용이하게 해준다.
    
    * `component`에 있는 `data`는 반드시 함수 형태여야 한다.
    - `props`는 부모 객체에 있는 `data`를 child(component)에 보낼 때 사용된다.
    - component는 자신이 받는 `props`의 상세사항들을 설정할 수 있다. (type, required, etc)

--- main.js
+++ main.js
@@ -1,53 +1,127 @@
+Vue.component("product", {
+	props: {
+		premium: {
+			type: Boolean,
+			required: true,
+		},
+	},
+	template: `
+    <div class="product">
+    <div class="product-image">
+      <img v-bind:src="image">
+    </div>
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <span v-show="onSale" 
+            style="background-color: red; 
+                  color:white; 
+                  padding: 5px; 
+                  display: block; 
+                  text-align: center; 
+                  font-weight: 600;">
+                  On Sale!
+      </span>
+      <p v-if="inStock > 10" 
+        style="font-weight: 600;">
+        In Stock
+      </p>
+      <p v-else-if="inStock <= 10 && inStock > 0">
+        <strong>Almost sold out!</strong>
+      </p>
+      <p v-else 
+          style="color: red; 
+                font-weight: 600;">
+                Out of Stock
+      </p>
+      <p>Shipping: {{ shipping }}</p>
+      <p>{{ description }}</p>
+      <ul style="margin-top: 10px;">
+        <li v-for="detail in details" style="margin: 5px 0px;">{{ detail }}</li>
+      </ul>
+
+      <div v-for="(variant, index) in variants" 
+            :key="variant.variantId" 
+            class="color-box"
+            :style="{ backgroundColor: variant.variantColor }" 
+            @mouseover="updateProduct(index)">
+      </div>
+      <a :href="link" target="_blank">Go to Website</a>
+
+      <button v-on:click="addToCart" 
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }">Add to Cart</button>
+      
+      <button @click="removeFromCart" style="background-color: pink;">Remove from Cart</button>
+
+      <div class="cart">
+        <p>Cart({{cart}})</p>
+      </div>
+    </div>
+  </div>
+  `,
+	data() {
+		return {
+			brand: "Rhange",
+			product: "Socks",
+			description: "This socks is very popular.",
+			selectedVariant: 0,
+			link: "https://www.softwear.com.ng/product/white-socks",
+			inventory: 100,
+			onSale: true,
+			details: ["80% cotton", "20% polyester", "Gender-neutral"],
+			variants: [
+				{
+					variantId: 2234,
+					variantColor: "white",
+					variantImage: "./assets/white-socks.jpg",
+					variantQuantity: 10,
+				},
+				{
+					variantId: 2235,
+					variantColor: "black",
+					variantImage: "./assets/black-socks.jpg",
+					variantQuantity: 0,
+				},
+			],
+			cart: 0,
+		};
+	},
+	methods: {
+		addToCart: function () {
+			this.cart += 1;
+		},
+		updateProduct(index) {
+			this.selectedVariant = index;
+			console.log(index);
+		},
+		removeFromCart: function () {
+			if (this.cart > 0) {
+				this.cart -= 1;
+			}
+		},
+	},
+	computed: {
+		title() {
+			return `${this.brand} ${this.product}`;
+		},
+		image() {
+			return this.variants[this.selectedVariant].variantImage;
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity;
+		},
+		shipping() {
+			if (this.premium) {
+				return "Free";
+			}
+			return 2.99;
+		},
+	},
+});
+
 let app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Rhange',
-    product: 'Socks',
-    description: 'This socks is very popular.',
-    selectedVariant: 0,
-    link: 'https://www.softwear.com.ng/product/white-socks',
-    inventory: 100,
-    onSale: true,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "white",
-        variantImage: './assets/white-socks.jpg',
-        variantQuantity: 10
-      },
-      {
-        variantId: 2235,
-        variantColor: "black",
-        variantImage: './assets/black-socks.jpg',
-        variantQuantity: 0
-      }
-    ],
-    cart: 0
-  },
-  methods: {
-    addToCart: function () {
-      this.cart += 1
-    },
-    updateProduct(index) {
-      this.selectedVariant = index
-      console.log(index)
-    },
-    removeFromCart: function () {
-      if (this.cart > 0 ) {
-        this.cart -= 1
-      }
-    }
-  },
-  computed: {
-    title() {
-      return `${this.brand} ${this.product}`
-    },
-    image() {
-      return this.variants[this.selectedVariant].variantImage
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].variantQuantity
-    }
-  }
-})
+	el: "#app",
+	data: {
+		premium: true,
+	},
+});
